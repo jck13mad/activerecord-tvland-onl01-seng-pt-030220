d59037a469a51cf8201018b1e69b99706c620cab
Jack Madden <jck13mad@gmail.com>
2020-05-31 00:23:04 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
   
+  belongs_to :network
+  has_many :characters, :actors 
+  
+  def actors_list
+    self.actors.collect do |act|
+      "#{act.name} - #{act.show.name}"
+    end
+  end
+  
 end
