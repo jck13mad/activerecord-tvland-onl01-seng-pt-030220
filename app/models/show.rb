a98150ebcb6a80284267a5fb7bc6b9d3ad5b2842
Jack Madden <jck13mad@gmail.com>
2020-05-31 00:34:41 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,7 +6,7 @@ class Show < ActiveRecord::Base
   
   def actors_list
     self.characters.collect do |act|
-      "Andrew Lincoln - #{act.show.name}"
+      "#{act.name} - #{act.show.name}"
     end
   end
   
