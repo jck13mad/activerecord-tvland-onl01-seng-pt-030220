3ae26bec13efbb847f1a97ef83c691178c1eb52f
Jack Madden <jck13mad@gmail.com>
2020-05-31 00:37:54 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,13 +1,13 @@
-class Show < ActiveRecord::Base
+# class Show < ActiveRecord::Base
   
-  belongs_to :network
-  has_many :characters
-  has_many :actors,  through: :characters
+#   belongs_to :network
+#   has_many :characters
+#   has_many :actors,  through: :characters
   
-  def actors_list
-    self.characters.collect do |act|
-      "#{act.name} - #{act.show.name}"
-    end
-  end
+#   def actors_list
+#     self.characters.collect do |act|
+#       "#{act.name} - #{act.show.name}"
+#     end
+#   end
   
-end
+# end
